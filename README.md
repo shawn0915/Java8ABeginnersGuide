39704ba2a4e0d85c79290caa431394820b4770aa
shawn <shawn2016@aliyun.com>
2017-03-26 23:37:59 +0800

    init import.

---
+++ README.md
@@ -0,0 +1,12 @@
+#Java, A Beginner's Guide, Sixth Edition 读书笔记
+
+# Java SE 8 
+
+Java Platform Standard Edition 8
+
+# src
+https://www.mhprofessional.com/oraclepressbooks/downloads_list
+
+# jdk8 新特性
+lambda表达式
+
---
+++ chapter_01/Example.java
@@ -0,0 +1,12 @@
+/*
+    Example.java
+ */
+class Example{
+    public static void main(String args[]){
+        System.out.println("Java drives the web.");
+    }
+}
+
+//javac Example.java
+//java Example
+//#=>Java drives the web.
